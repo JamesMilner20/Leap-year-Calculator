7e57773032fa64c8ba244c7f512275541a216bfb
jamesmilner20 <jamesduah20@gmail.com>
2017-09-20 15:21:52 +0000

    employing the use of || and 8312 to make the code shorter

--- js/scripts.js
+++ js/scripts.js
@@ -7,26 +7,21 @@ $(document).ready(function() {
     });
   });
 
+  var leapYear = function(year) {
+    if ((year % 4 === 0) && (year % 100 !== 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
-  // var leapYear = function(year) {
-  //     return false;
-  //   };
-  //
-  //   var leapYear = function(year) {
-  //       if (year % 4 === 0) {
-  //         return true;
-  //       } else {
-  //         return false;
-  //       }
-  //     };
-
-      var leapYear = function(year) {
-          if (year % 100 === 0) {
-            return false;
-          } else if (year % 4 === 0) {
-            return true;
-          } else {
-            return false;
-          }
-        };
+      // var leapYear = function(year) {
+      //     if (year % 100 === 0) {
+      //       return false;
+      //     } else if (year % 4 === 0) {
+      //       return true;
+      //     } else {
+      //       return false;
+      //     }
+      //   };
     // jQuery user interface logic here
