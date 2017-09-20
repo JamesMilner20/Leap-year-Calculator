89cd5af921a468c1c2eca8fc3430993ccca16cc3
jamesmilner20 <jamesduah20@gmail.com>
2017-09-20 15:14:21 +0000

    if year is divisible by 100 return false added

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,25 @@ $(document).ready(function() {
   });
 
 
-  var leapYear = function(year) {
-      return false;
-    };
+  // var leapYear = function(year) {
+  //     return false;
+  //   };
+  //
+  //   var leapYear = function(year) {
+  //       if (year % 4 === 0) {
+  //         return true;
+  //       } else {
+  //         return false;
+  //       }
+  //     };
 
-    var leapYear = function(year) {
-        if (year % 4 === 0) {
-          return true;
-        } else {
-          return false;
-        }
-      };
+      var leapYear = function(year) {
+          if (year % 100 === 0) {
+            return false;
+          } else if (year % 4 === 0) {
+            return true;
+          } else {
+            return false;
+          }
+        };
     // jQuery user interface logic here
